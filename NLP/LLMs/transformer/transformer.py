799f6d003fa9cf2941b3587da426b8b6c83ddc2b
adityapaikrao <paikrao.aditya@gmail.com>
2026-04-15 23:24:42 -0400

    add implementation of embedding and positional encoding in Transformer class

--- NLP/LLMs/transformer/transformer.py
+++ NLP/LLMs/transformer/transformer.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 from typing import Optional, Tuple, Type
 from .attention.attention import SingleHeadAttention
-        
+from .embedding.tokenEmbedding import TokenEmbedding
+from .embedding.positionEmbedding import PositionalEncoding
 
 class Transformer(nn.Module):
     def __init__(self, 
@@ -13,4 +14,6 @@ class Transformer(nn.Module):
                  max_len: int,
                  attn: Type[nn.Module]
                 ):
-        pass
+        super().__init__()
+        self.embedding = TokenEmbedding(vocab_size, d_model)
+        self.pos_embedding = PositionalEncoding(max_len, d_model)
