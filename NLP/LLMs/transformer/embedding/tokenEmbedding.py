30a8f36b8ff1579ab377c3072d3ccc9e94492750
adityapaikrao <paikrao.aditya@gmail.com>
2026-04-15 22:01:50 -0400

    add TokenEmbedding class for standard token embedding layer in Transformer models

---
+++ NLP/LLMs/transformer/embedding/tokenEmbedding.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch
+
+class TokenEmbedding(nn.Module):
+    """
+    Standard token embedding layer for Transformer models.
+    Converts input tokens into dense vectors of size d_model.
+    """
+    def __init__(
+            self, 
+            vocab_size: int, 
+            d_model: int
+        ):
+        """
+        Initializes the TokenEmbedding.
+
+        Args:
+            vocab_size (int): Size of the vocabulary.
+            d_model (int): Dimension of the embedding vectors.
+        """
+        super().__init__()
+        self.embedding_VD = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the token embedding layer.
+
+        Args:
+            x (torch.Tensor): Input tensor containing token indices.
+
+        Returns:
+            torch.Tensor: Embedded output tensor.
+        """
+        return self.embedding_VD(x)
