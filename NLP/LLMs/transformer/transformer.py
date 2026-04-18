3868d43e3ed90423da8b86e6f57c306ab26d77bf
adityapaikrao <paikrao.aditya@gmail.com>
2026-04-18 15:47:55 -0400

    add TransformerEncoderBlock class with multi-head attention and feed-forward network

--- NLP/LLMs/transformer/transformer.py
+++ NLP/LLMs/transformer/transformer.py
@@ -1,19 +1,56 @@
 import torch 
 import torch.nn as nn
-from typing import Optional, Tuple, Type
-from .attention.attention import SingleHeadAttention
-from .embedding.tokenEmbedding import TokenEmbedding
-from .embedding.positionEmbedding import PositionalEncoding
+from typing import Type, Optional
+from .feedforward import FeedForward
 
-class Transformer(nn.Module):
+class TransformerEncoderBlock(nn.Module):
+    """
+    A single Transformer block consisting of multi-head attention and a feed-forward network.
+
+    Args:
+        vocab_size (int): Size of the dictionary of embeddings.
+        d_model (int): The number of expected features in the input.
+        num_heads (int): The number of heads in the multiheadattention models.
+        d_ff (int): The dimension of the feedforward network model.
+        max_len (int): The maximum sequence length.
+        attn (Type[nn.Module]): Multi-head attention layer class.
+    """
     def __init__(self, 
-                 vocab_size: int, 
+                 attn: Type[nn.Module],
                  d_model: int,
-                 num_heads: int,
-                 d_ff: int,
-                 max_len: int,
-                 attn: Type[nn.Module]
+                 d_ff: Optional[int] = None,
+                 num_heads: int = 1
                 ):
         super().__init__()
-        self.embedding = TokenEmbedding(vocab_size, d_model)
-        self.pos_embedding = PositionalEncoding(max_len, d_model)
+
+
+        self.attention = attn(d_model)
+        self.feed_forward = FeedForward(d_model, d_ff)
+
+        self.layer_norm1 = nn.LayerNorm(d_model)
+        self.layer_norm2 = nn.LayerNorm(d_model)
+    
+    def forward(self, x_BTD: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the Transformer block.
+
+        Args:
+            x_BTD (torch.Tensor): Input tensor of shape (batch_size, sequence_length, d_model).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (batch_size, sequence_length, d_model).
+        """
+
+        # Attention Block
+        t_BTD = self.layer_norm1(x_BTD)
+        t_BTD = self.attention(t_BTD)
+        x_BTD = x_BTD + t_BTD
+
+        # FeedForward Block
+        t_BTD = self.layer_norm2(x_BTD)
+        t_BTD = self.feed_forward(t_BTD)
+        x_BTD = x_BTD + t_BTD
+
+        return x_BTD
+        
+
